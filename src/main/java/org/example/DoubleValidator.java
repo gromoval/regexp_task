bb2b681dc93387d681075dbb770dd6bef4a35450
gromoval <gromov-rabota@yandex.ru>
2020-04-02 19:51:37 +0300

    [+] almost all cases work fine!

--- src/main/java/org/example/DoubleValidator.java
+++ src/main/java/org/example/DoubleValidator.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class DoubleValidator implements Validator {
     public boolean isValid(String value) {
-        return false;
+        return value.matches("^[-+]?[0-9_]*\\.?[0-9_]*[d_]?$");
     }
 }
--- src/main/java/org/example/PhoneValidator.java
+++ src/main/java/org/example/PhoneValidator.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class PhoneValidator implements Validator {
     public boolean isValid(String value) {
-        return false;
+//        return value.matches("^[+]?[0-9()-]{6,}$");
+        return value.matches("^((\\+?\\d{1,3})[- ]?)?([(| |-]\\d{2,4}[)| |-])?[\\d\\- ]{6,12}(\\(([а-я.]*\\s?)?\\d{2,3}\\))?$");
     }
 }
